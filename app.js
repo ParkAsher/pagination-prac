d85b7e86202467896959c41f9d9ac82e28337b5b
ParkAsher <devasherpark@naver.com>
2023-01-26 23:36:51 +0900

    페이지네이션

--- app.js
+++ app.js
@@ -9,5 +9,61 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/views'));
 
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+
+/* api */
+app.get('/movies', (req, res) => {
+    let currentPage = req.query.page;
+    if (currentPage === 'null') currentPage = 1;
+
+    let Movies = [...movies];
+
+    // 총 게시글 수
+    let totalPostLength = Movies.length;
+    // console.log(totalPostLength);
+
+    // 총 페이지 수 : 1페이지당 10개씩
+    let totalPage = Math.ceil(totalPostLength / 10);
+    // console.log(totalPage);
+
+    // 화면에 보여줄 그룹 : 한 그룹당 6개 페이지 띄우기
+    let pageGroup = Math.ceil(currentPage / 6);
+
+    // 한 그룹의 마지막 페이지번호
+    let lastPage = pageGroup * 6;
+
+    // 한 그룹의 첫 페이지번호
+    let firstPage = lastPage - 6 + 1 <= 0 ? 1 : lastPage - 6 + 1;
+
+    if (lastPage > totalPage) {
+        lastPage = totalPage;
+    }
+
+    // 게시글 잘라서 가져오기
+    const paginationMovies = Movies.splice((currentPage - 1) * 10, 10);
+
+    paginationMovies.sort((a, b) => {
+        const prevTime = new Date(a.created_at).getTime();
+        const nextTime = new Date(b.created_at).getTime();
+        return nextTime - prevTime;
+    });
+
+    console.log(firstPage, lastPage);
+
+    res.send({
+        pageInfo: {
+            firstPage,
+            lastPage,
+        },
+        movies: paginationMovies,
+    });
+});
+
+app.listen(5000, () => {
+    console.log(`5000포트에서 실행`);
+});
